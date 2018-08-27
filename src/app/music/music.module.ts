1b9245a57067d5b08f15c4ec60831beaa323a7bd
Muata Nkosi <mnkosi@fandm.edu>
2018-08-27 01:04:37 -0400

    music-search component added

--- src/app/music/music.module.ts
+++ src/app/music/music.module.ts
@@ -7,12 +7,12 @@ import { AutoCompleteModule } from 'primeng/primeng';
 
 // Custom imports
 import { MusicSearchComponent } from './music-search/music-search.component';
-import { MusicPlayerComponent } from './music-player/music-player.component';
-import { MusicDetailsComponent } from './music-details/music-details.component';
-import { MusicProgressComponent } from './music-progress/music-progress.component';
-import { MusicFooterComponent } from './music-footer/music-footer.component';
-import { MusicService } from './shared/music.service';
-import { ApiService } from './shared/api.service';
+// import { MusicPlayerComponent } from './music-player/music-player.component';
+// import { MusicDetailsComponent } from './music-details/music-details.component';
+// import { MusicProgressComponent } from './music-progress/music-progress.component';
+// import { MusicFooterComponent } from './music-footer/music-footer.component';
+// import { MusicService } from './shared/music.service';
+// import { ApiService } from './shared/api.service';
 
 @NgModule({
   imports: [
@@ -24,23 +24,23 @@ import { ApiService } from './shared/api.service';
   exports: [
     // Expose components
     MusicSearchComponent,
-    MusicDetailsComponent,
-    MusicPlayerComponent,
-    MusicProgressComponent,
-    MusicFooterComponent
+    // MusicDetailsComponent,
+    // MusicPlayerComponent,
+    // MusicProgressComponent,
+    // MusicFooterComponent
   ],
   declarations: [
     // Declare components
     MusicSearchComponent,
-    MusicDetailsComponent,
-    MusicPlayerComponent,
-    MusicProgressComponent,
-    MusicFooterComponent
+    // MusicDetailsComponent,
+    // MusicPlayerComponent,
+    // MusicProgressComponent,
+    // MusicFooterComponent
   ],
   providers: [
     // Services
-    ApiService,
-    MusicService
+    // ApiService,
+    // MusicService
   ]
 })
 export class MusicModule { }
